673329f1f1258d86949a02d9977bc50a37f89f2a
shubhamsinha1010 <sinhashubham1911@gmail.com>
2023-10-09 18:28:48 +0530

    fix: added apis that send message from one microservice to the other

--- test_func.py
+++ test_func.py
@@ -1,9 +1,54 @@
+import pytest
 from fastapi.testclient import TestClient
-from app import app
+from app import app  # Assuming your FastAPI app is in a file named 'app.py'
+from utils.producer import RabbitMQProducer
+from utils.consumer import RabbitMQConsumer
 
+# Create a test client for the FastAPI app
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
+# Define test data
+message_data = {
+    "message_from": "user1",
+    "message_to": "user2",
+    "message": "Hello, World!",
+}
+
+# Define a mock RabbitMQ producer and consumer for testing
+class MockRabbitMQProducer(RabbitMQProducer):
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def send_message(self, message):
+        pass
+
+class MockRabbitMQConsumer(RabbitMQConsumer):
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def start_consuming(self):
+        return message_data  # Mocked message for testing
+
+# Replace the original producers and consumers with the mock ones
+app.dependency_overrides[RabbitMQProducer] = MockRabbitMQProducer
+app.dependency_overrides[RabbitMQConsumer] = MockRabbitMQConsumer
+
+def test_send_message():
+    response = client.post("/send-message", json=message_data)
     assert response.status_code == 200
-    assert response.json() == {"message": "HELLO WORLD"}
+    assert response.json() == "Message Sent Successfully"
+
+def test_get_message():
+    response = client.get("/get-message")
+    assert response.status_code == 200
+    assert response.json() == message_data
+
+# Run tests with pytest
+if __name__ == '__main__':
+    pytest.main()
